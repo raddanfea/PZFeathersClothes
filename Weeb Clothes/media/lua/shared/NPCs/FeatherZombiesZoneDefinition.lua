45680d9531fd757b82150247de04c38c6f6f5ccd
feather <feather@FEA>
2022-01-16 15:16:00 +0100

    removing MP update items and new loot distribution

---
+++ Weeb Clothes/media/lua/shared/NPCs/FeatherZombiesZoneDefinition.lua
@@ -0,0 +1,28 @@
+require 'NPCs/ZombiesZoneDefinition'
+
+table.insert(ZombiesZoneDefinition.Default,{name="FeatherThighs1",chance=3,});
+table.insert(ZombiesZoneDefinition.Default,{name="FeatherThighs2",chance=3,});
+table.insert(ZombiesZoneDefinition.Default,{name="FeatherThighs3",chance=3,});
+table.insert(ZombiesZoneDefinition.Default,{name="FeatherThighs4",chance=3,});
+table.insert(ZombiesZoneDefinition.Default,{name="FeatherThighs5",chance=3,});
+table.insert(ZombiesZoneDefinition.Default,{name ="FeatherBunny", chance=1, room="bedroom"});
+table.insert(ZombiesZoneDefinition.Default,{name ="FeatherFox", chance=1, room="bedroom"});
+
+table.insert(ZombiesZoneDefinition.Rocker,{name="FeatherThighs1",chance=2,});
+table.insert(ZombiesZoneDefinition.Rocker,{name="FeatherThighs2",chance=2,});
+table.insert(ZombiesZoneDefinition.Rocker,{name="FeatherThighs3",chance=2,});
+table.insert(ZombiesZoneDefinition.Rocker,{name="FeatherThighs4",chance=2,});
+table.insert(ZombiesZoneDefinition.Rocker,{name="FeatherThighs5",chance=2,});
+
+table.insert(ZombiesZoneDefinition.StreetSports,{name="FeatherThighs1",chance=1,});
+table.insert(ZombiesZoneDefinition.StreetSports,{name="FeatherThighs2",chance=1,});
+table.insert(ZombiesZoneDefinition.StreetSports,{name="FeatherThighs3",chance=1,});
+table.insert(ZombiesZoneDefinition.StreetSports,{name="FeatherThighs4",chance=1,});
+
+table.insert(ZombiesZoneDefinition.School,{name="FeatherThighs1",chance=15,});
+table.insert(ZombiesZoneDefinition.School,{name="FeatherThighs2",chance=15,});
+table.insert(ZombiesZoneDefinition.School,{name="FeatherThighs3",chance=15,});
+table.insert(ZombiesZoneDefinition.School,{name="FeatherThighs4",chance=15,});
+
+table.insert(ZombiesZoneDefinition.Stripclub,{name ="FeatherBunny", chance=2,});
+table.insert(ZombiesZoneDefinition.Stripclub,{name ="FeatherFox", chance=2,});
