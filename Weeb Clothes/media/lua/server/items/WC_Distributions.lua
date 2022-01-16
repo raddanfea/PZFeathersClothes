dd049b4bcb99dfedc2b62bbbc8c1428fb0b57514
feather <feather@FEA>
2022-01-16 15:14:38 +0100

    removing MP update items and new loot distribution

--- Weeb Clothes/media/lua/server/items/WC_Distributions.lua
+++ Weeb Clothes/media/lua/server/items/WC_Distributions.lua
@@ -1,237 +1,207 @@
-Distributions = Distributions or {};
+require 'Items/ProceduralDistributions'
 
-local distributionTable = 
-{
+--BedroomDresser
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Base.VeryLongGloves");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 2);
 
-    zippeestore =
-    {
-        counter = {
-            rolls = 1,
-            items = {
-            }
-        },
-    },
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Base.ThighHighSocks");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 8);
 
-    gigamart =
-    {
-        
-        counter ={
-            rolls = 1,
-            items = {
-            }
-        },
-    },
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Base.Shoes_ThighHighBoots");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 2);
 
-    fossoil =
-    {
-        counter ={
-            rolls = 1,
-            items = {
-            }
-        },
-    },
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Base.FoxTail");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
 
-    bedroom =
-    {
-        
-        sidetable = {
-            rolls = 1,
-            items = {
-                "Base.Corset", 0.5,
-                "Base.Miniskirt", 0.5,
-                "Base.VeryLongGloves", 0.5,
-                "Base.ThighHighSocks", 0.8,
-                "Base.Shoes_ThighHighBoots", 0.5,
-                "Base.FoxTail", 0.1,
-                "Base.FoxEars", 0.1,
-                "Base.BunnySuit", 0.1,
-            }
-        },
-    },
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Base.FoxEars");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
 
-    optometrist = {
-        counter = {
-            rolls = 3,
-            items = {
-            }
-        },
-    },
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Base.BunnySuit");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
 
-	aesthetic = {
-        counter ={
-            rolls = 1,
-            items = {
-            },
-        },
-    },
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Base.Hat_BunnyEarsBlack");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
 
-	clothesstore = {
+--DresserGeneric
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Base.VeryLongGloves");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 5);
 
-        counter ={
-            rolls = 3,
-            items = {
-				"Base.Corset", 0.5,
-                "Base.Miniskirt", 0.5,
-                "Base.VeryLongGloves", 0.5,
-                "Base.ThighHighSocks", 0.8,
-                "Base.Shoes_ThighHighBoots", 0.5,
-                "Base.FoxTail", 0.1,
-                "Base.FoxEars", 0.1,
-                "Base.BunnySuit", 0.1,
-            },
-        },
-    },
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Base.ThighHighSocks");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 8);
 
-	
-	gunstore = {
-        counter ={
-            rolls = 3,
-            items = {
-            },
-            dontSpawnAmmo = true,
-        },
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Base.Shoes_ThighHighBoots");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 4);
 
-        displaycase ={
-            rolls = 3,
-            items = {
-            },
-            dontSpawnAmmo = true,
-        },
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Base.FoxTail");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 0.1);
 
-        locker ={
-            rolls = 3,
-            items = {
-            },
-            dontSpawnAmmo = true,
-        },
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Base.FoxEars");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 0.1);
 
-        metal_shelves ={
-            rolls = 2,
-            items = {
-            },
-            dontSpawnAmmo = true,
-        },
-    },
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Base.BunnySuit");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 0.1);
 
-    gunstorestorage ={
-        all={
-            rolls = 2,
-            items = {
-            },
-    
-            dontSpawnAmmo = true,
-        },
-    },
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Base.Hat_BunnyEarsBlack");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 0.1);
 
-    armysurplus = {
-        shelves = {
-            rolls = 2,
-            items = {
-            },
-        },
-        
-        metal_shelves = {
-            rolls = 2,
-            items = {
-            },
-        },
-        
-        
-        clothingrack = {
-            rolls = 3,
-            items = {
-            },
-        },
-    },
-    
-    armyhanger = {
-        metal_shelves = {
-            rolls = 3,
-            items = {
-            },
-        },
-    },
+--WardrobeWoman
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Base.VeryLongGloves");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 4);
 
-    sportstore = {
-        shelves = {
-            rolls = 4,
-            items = {
-            },
-        },
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Base.ThighHighSocks");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 5);
 
-        clothingrack = {
-            rolls = 3,
-            items = {
-            },
-        },
-    },
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Base.Shoes_ThighHighBoots");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 1);
 
-    policestorage = {
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Base.FoxTail");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.2);
 
-        locker = {
-            rolls = 2,
-            items = {
-            },
-        },
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Base.FoxEars");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.2);
 
-        metal_shelves = {
-            rolls = 2,
-            items = {
-            },
-        },
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Base.BunnySuit");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.2);
 
-        counter = {
-            rolls = 2,
-            items = {
-            },
-        },
-    },
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Base.Hat_BunnyEarsBlack");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.1);
 
-    medclinic =
-    {
-        isShop = true,
-        counter =
-        {
-            rolls = 1,
-            items = {
-            }
-        }
-    },
-    
-    medicalstorage = {
-        isShop = true,
-        metal_shelves =
-        {
-            rolls = 1,
-            items = {
-            }
-        }
-    },
+--ClosetShelfGeneric
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, "Base.Shoes_ThighHighBoots");
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, 6);
 
-    pharmacystorage = {
-        isShop = true,
-        metal_shelves ={
-            rolls = 1,
-            items = {
-            }
-        },
-    },
+--ClothingStoresBoots
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, "Base.Shoes_ThighHighBoots");
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, 6);
 
-    all = {
+--ClothingStoresGloves
+table.insert(ProceduralDistributions.list.ClothingStoresGloves.items, "Base.VeryLongGloves");
+table.insert(ProceduralDistributions.list.ClothingStoresGloves.items, 6);
 
-        inventorymale = {
-            rolls = 1,
-            items = {
-            }
-        },
-        inventoryfemale = {
-            rolls = 1,
-            items = {
-            }
-            
-        },
-    },
+--ClothingStoresSocks
+table.insert(ProceduralDistributions.list.ClothingStoresSocks.items, "Base.ThighHighSocks");
+table.insert(ProceduralDistributions.list.ClothingStoresSocks.items, 8);
 
-}
+--CrateClothesRandom
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Base.VeryLongGloves");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 3);
 
-table.insert(Distributions, distributionTable);
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Base.ThighHighSocks");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 8);
+
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Base.Shoes_ThighHighBoots");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 2);
+
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Base.FoxTail");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 2);
+
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Base.FoxEars");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 2);
+
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Base.BunnySuit");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 2);
+
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Base.Hat_BunnyEarsBlack");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 2);
+
+--GymLockers
+table.insert(ProceduralDistributions.list.GymLockers.items, "Base.ThighHighSocks");
+table.insert(ProceduralDistributions.list.GymLockers.items, 2);
+
+--LaundryLoad1
+table.insert(ProceduralDistributions.list.LaundryLoad1.items, "Base.VeryLongGloves");
+table.insert(ProceduralDistributions.list.LaundryLoad1.items, 8);
+
+table.insert(ProceduralDistributions.list.LaundryLoad1.items, "Base.ThighHighSocks");
+table.insert(ProceduralDistributions.list.LaundryLoad1.items, 10);
+
+table.insert(ProceduralDistributions.list.LaundryLoad1.items, "Base.FoxTail");
+table.insert(ProceduralDistributions.list.LaundryLoad1.items, 0.5);
+
+table.insert(ProceduralDistributions.list.LaundryLoad1.items, "Base.FoxEars");
+table.insert(ProceduralDistributions.list.LaundryLoad1.items, 0.5);
+
+table.insert(ProceduralDistributions.list.LaundryLoad1.items, "Base.BunnySuit");
+table.insert(ProceduralDistributions.list.LaundryLoad1.items, 0.5);
+
+table.insert(ProceduralDistributions.list.LaundryLoad1.items, "Base.Hat_BunnyEarsBlack");
+table.insert(ProceduralDistributions.list.LaundryLoad1.items, 0.5);
+
+--LaundryLoad7
+table.insert(ProceduralDistributions.list.LaundryLoad7.items, "Base.VeryLongGloves");
+table.insert(ProceduralDistributions.list.LaundryLoad7.items, 8);
+
+table.insert(ProceduralDistributions.list.LaundryLoad7.items, "Base.ThighHighSocks");
+table.insert(ProceduralDistributions.list.LaundryLoad7.items, 10);
+
+table.insert(ProceduralDistributions.list.LaundryLoad7.items, "Base.FoxTail");
+table.insert(ProceduralDistributions.list.LaundryLoad7.items, 0.5);
+
+table.insert(ProceduralDistributions.list.LaundryLoad7.items, "Base.FoxEars");
+table.insert(ProceduralDistributions.list.LaundryLoad7.items, 0.5);
+
+table.insert(ProceduralDistributions.list.LaundryLoad7.items, "Base.BunnySuit");
+table.insert(ProceduralDistributions.list.LaundryLoad7.items, 0.5);
+
+table.insert(ProceduralDistributions.list.LaundryLoad7.items, "Base.Hat_BunnyEarsBlack");
+table.insert(ProceduralDistributions.list.LaundryLoad7.items, 0.5);
+
+--LaundryLoad8
+table.insert(ProceduralDistributions.list.LaundryLoad8.items, "Base.VeryLongGloves");
+table.insert(ProceduralDistributions.list.LaundryLoad8.items, 8);
+
+table.insert(ProceduralDistributions.list.LaundryLoad8.items, "Base.ThighHighSocks");
+table.insert(ProceduralDistributions.list.LaundryLoad8.items, 10);
+
+table.insert(ProceduralDistributions.list.LaundryLoad8.items, "Base.FoxTail");
+table.insert(ProceduralDistributions.list.LaundryLoad8.items, 0.5);
+
+table.insert(ProceduralDistributions.list.LaundryLoad8.items, "Base.FoxEars");
+table.insert(ProceduralDistributions.list.LaundryLoad8.items, 0.5);
+
+table.insert(ProceduralDistributions.list.LaundryLoad8.items, "Base.BunnySuit");
+table.insert(ProceduralDistributions.list.LaundryLoad8.items, 0.5);
+
+table.insert(ProceduralDistributions.list.LaundryLoad8.items, "Base.Hat_BunnyEarsBlack");
+table.insert(ProceduralDistributions.list.LaundryLoad8.items, 0.5);
+
+--Locker
+table.insert(ProceduralDistributions.list.Locker.items, "Base.VeryLongGloves");
+table.insert(ProceduralDistributions.list.Locker.items, 0.2);
+
+table.insert(ProceduralDistributions.list.Locker.items, "Base.ThighHighSocks");
+table.insert(ProceduralDistributions.list.Locker.items, 1);
+
+table.insert(ProceduralDistributions.list.Locker.items, "Base.Shoes_ThighHighBoots");
+table.insert(ProceduralDistributions.list.Locker.items, 0.5);
+
+table.insert(ProceduralDistributions.list.Locker.items, "Base.FoxTail");
+table.insert(ProceduralDistributions.list.Locker.items, 0.01);
+
+table.insert(ProceduralDistributions.list.Locker.items, "Base.FoxEars");
+table.insert(ProceduralDistributions.list.Locker.items, 0.01);
+
+table.insert(ProceduralDistributions.list.Locker.items, "Base.BunnySuit");
+table.insert(ProceduralDistributions.list.Locker.items, 0.01);
+
+table.insert(ProceduralDistributions.list.Locker.items, "Base.Hat_BunnyEarsBlack");
+table.insert(ProceduralDistributions.list.Locker.items, 0.01);
+
+--StripClubDressers
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Base.VeryLongGloves");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Base.ThighHighSocks");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Base.Shoes_ThighHighBoots");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Base.FoxTail");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Base.FoxEars");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Base.BunnySuit");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
